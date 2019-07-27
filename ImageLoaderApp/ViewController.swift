97c140cacfe551ef2dc9538e11bea5d7432ead4e
Karthi <karthi.anandhan@freshworks.com>
2019-07-27 13:39:07 +0530

    Initial Commit

---
+++ ImageLoaderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageLoaderApp
+//
+//  Created by Karthi Anandhan on 27/07/19.
+//  Copyright © 2019 karthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
